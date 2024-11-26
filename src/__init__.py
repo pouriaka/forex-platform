86de63634501e7b146aace62987006b59c28beb1
Pouria <58914565+pouriaka@users.noreply.github.com>
2024-11-26 15:48:05 +0330

    __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ from .Metatrader import *  #It will import all the things in code1
 from .Datamine import *
 
 username = 51735591  # Username must be an int
-pasword = 'Wy7v5r4hu'  # Password must be a string
+pasword = '********'  # Password must be a string
 server = 'Alpari-MT5-Demo' # Server must be a string
 
 print('this will run when src use')
