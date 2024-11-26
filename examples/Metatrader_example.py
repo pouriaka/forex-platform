8067cf05616bb3bd66f6cab724130d108af80562
Pouria <58914565+pouriaka@users.noreply.github.com>
2024-11-26 15:56:29 +0330

    Update Metatrader_example.py

--- examples/Metatrader_example.py
+++ examples/Metatrader_example.py
@@ -1,5 +1,5 @@
 from src.Metatrader import *
 
 #ID shold be int and other should be str
-login = metatrader(11965591,'Wy7v8j9hu','Alpari-MT5-Demo')
+login = metatrader(11965591,'**********','Alpari-MT5-Demo')
 login.start_mt5()
